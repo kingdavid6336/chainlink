02e78bb9742284311204e55cf147f118c9997ff5
John Barker <dev@j16r.net>
2019-09-19 13:29:21 -0600

    Use channels instead of raw Calls to avoid data race

--- core/services/scheduler_test.go
+++ core/services/scheduler_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/onsi/gomega"
 	"github.com/smartcontractkit/chainlink/core/internal/cltest"
 	"github.com/smartcontractkit/chainlink/core/internal/mocks"
 	"github.com/smartcontractkit/chainlink/core/services"
@@ -23,15 +22,22 @@ func TestScheduler_Start_LoadingRecurringJobs(t *testing.T) {
 	jobWoCron := cltest.NewJob()
 	require.NoError(t, store.CreateJob(&jobWoCron))
 
+	executeJobChannel := make(chan struct{})
 	jm := new(mocks.JobManager)
-	jm.On("ExecuteJob", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil, nil).Twice()
+	jm.On("ExecuteJob", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(nil, nil).
+		Twice().
+		Run(func(mock.Arguments) {
+			executeJobChannel <- struct{}{}
+		})
 
 	sched := services.NewScheduler(store, jm)
 	require.NoError(t, sched.Start())
 
-	gomega.NewGomegaWithT(t).Eventually(func() int {
-		return len(jm.Calls)
-	}).Should(gomega.Equal(2))
+	cltest.CallbackOrTimeout(t, "ExecuteJob", func() {
+		<-executeJobChannel
+		<-executeJobChannel
+	}, 3*time.Second)
 
 	sched.Stop()
 
@@ -39,8 +45,14 @@ func TestScheduler_Start_LoadingRecurringJobs(t *testing.T) {
 }
 
 func TestRecurring_AddJob(t *testing.T) {
+	executeJobChannel := make(chan struct{}, 1)
 	jm := new(mocks.JobManager)
-	jm.On("ExecuteJob", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil, nil).Twice()
+	jm.On("ExecuteJob", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(nil, nil).
+		Run(func(mock.Arguments) {
+			executeJobChannel <- struct{}{}
+		}).
+		Twice()
 
 	r := services.NewRecurring(jm)
 	cron := cltest.NewMockCron()
@@ -51,15 +63,15 @@ func TestRecurring_AddJob(t *testing.T) {
 
 	cron.RunEntries()
 
-	gomega.NewGomegaWithT(t).Eventually(func() int {
-		return len(jm.Calls)
-	}).Should(gomega.Equal(1))
+	cltest.CallbackOrTimeout(t, "ExecuteJob", func() {
+		<-executeJobChannel
+	}, 3*time.Second)
 
 	cron.RunEntries()
 
-	gomega.NewGomegaWithT(t).Eventually(func() int {
-		return len(jm.Calls)
-	}).Should(gomega.Equal(2))
+	cltest.CallbackOrTimeout(t, "ExecuteJob", func() {
+		<-executeJobChannel
+	}, 3*time.Second)
 
 	r.Stop()
 
@@ -70,8 +82,14 @@ func TestOneTime_AddJob(t *testing.T) {
 	store, cleanup := cltest.NewStore(t)
 	defer cleanup()
 
+	executeJobChannel := make(chan struct{})
 	jm := new(mocks.JobManager)
-	jm.On("ExecuteJob", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil, nil).Once()
+	jm.On("ExecuteJob", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+		Return(nil, nil).
+		Once().
+		Run(func(mock.Arguments) {
+			executeJobChannel <- struct{}{}
+		})
 
 	clock := cltest.NewTriggerClock()
 
@@ -89,16 +107,15 @@ func TestOneTime_AddJob(t *testing.T) {
 
 	clock.Trigger()
 
-	gomega.NewGomegaWithT(t).Eventually(func() int {
-		return len(jm.Calls)
-	}).Should(gomega.Equal(1))
+	cltest.CallbackOrTimeout(t, "ws client restarts", func() {
+		<-executeJobChannel
+	}, 3*time.Second)
 
 	// This should block because if OneTime works it won't listen on the channel again
 	go clock.Trigger()
 
-	gomega.NewGomegaWithT(t).Consistently(func() int {
-		return len(jm.Calls)
-	}).Should(gomega.Equal(1))
+	// Sleep for some time to make sure another call isn't made
+	time.Sleep(1 * time.Second)
 
 	ot.Stop()
 
