a7e9ad7512a9271cc8f7dced7648bd18c884560c
John Barker <dev@j16r.net>
2019-10-02 16:11:14 -0600

    Add more tests for JobRunner, test cleanup, multiplexing (FAIL)

--- core/services/job_runner_test.go
+++ core/services/job_runner_test.go
@@ -3,6 +3,7 @@ package services_test
 import (
 	"testing"
 
+	"github.com/onsi/gomega"
 	"github.com/smartcontractkit/chainlink/core/internal/cltest"
 	"github.com/smartcontractkit/chainlink/core/internal/mocks"
 	"github.com/smartcontractkit/chainlink/core/services"
@@ -12,6 +13,7 @@ import (
 
 func TestJobRunner(t *testing.T) {
 	t.Parallel()
+	g := gomega.NewGomegaWithT(t)
 
 	je := new(mocks.JobExecutor)
 	jr := services.NewJobRunner(je)
@@ -29,11 +31,92 @@ func TestJobRunner(t *testing.T) {
 
 	jr.Run(&models.JobRun{ID: models.NewID()})
 
+	g.Eventually(func() int {
+		return jr.WorkerCount()
+	}).Should(gomega.Equal(1))
+
 	cltest.CallbackOrTimeout(t, "Execute", func() {
 		<-executeJobChannel
 	})
 
 	je.AssertExpectations(t)
 
-	//assert.Equal(t, 0, jr.workerCount())
+	g.Eventually(func() int {
+		return jr.WorkerCount()
+	}).Should(gomega.Equal(0))
+}
+
+func TestJobRunner_OneWorkerPerRun(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewGomegaWithT(t)
+
+	je := new(mocks.JobExecutor)
+	jr := services.NewJobRunner(je)
+
+	executeJobChannel := make(chan struct{})
+
+	jr.Start()
+	defer jr.Stop()
+
+	je.On("Execute", mock.Anything).
+		Return(nil, nil).
+		Run(func(mock.Arguments) {
+			executeJobChannel <- struct{}{}
+		})
+
+	jr.Run(&models.JobRun{ID: models.NewID()})
+	jr.Run(&models.JobRun{ID: models.NewID()})
+
+	g.Eventually(func() int {
+		return jr.WorkerCount()
+	}).Should(gomega.Equal(2))
+
+	cltest.CallbackOrTimeout(t, "Execute", func() {
+		<-executeJobChannel
+		<-executeJobChannel
+	})
+
+	je.AssertExpectations(t)
+
+	g.Eventually(func() int {
+		return jr.WorkerCount()
+	}).Should(gomega.Equal(0))
+}
+
+func TestJobRunner_OneWorkerForSameRunTriggeredMultipleTimes(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewGomegaWithT(t)
+
+	je := new(mocks.JobExecutor)
+	jr := services.NewJobRunner(je)
+
+	executeJobChannel := make(chan struct{})
+
+	jr.Start()
+	defer jr.Stop()
+
+	je.On("Execute", mock.Anything).
+		Return(nil, nil).
+		Run(func(mock.Arguments) {
+			executeJobChannel <- struct{}{}
+		})
+
+	id := models.NewID()
+	jr.Run(&models.JobRun{ID: id})
+	jr.Run(&models.JobRun{ID: id})
+
+	g.Eventually(func() int {
+		return jr.WorkerCount()
+	}).Should(gomega.Equal(1))
+
+	cltest.CallbackOrTimeout(t, "Execute", func() {
+		<-executeJobChannel
+		<-executeJobChannel
+	})
+
+	je.AssertExpectations(t)
+
+	g.Eventually(func() int {
+		return jr.WorkerCount()
+	}).Should(gomega.Equal(0))
 }
