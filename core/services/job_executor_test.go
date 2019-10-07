24da1ed2d634dc3e4ebf745753b414d0b99ef59f
John Barker <dev@j16r.net>
2019-10-07 16:22:27 +0900

    Assert payment is recorded in JobExecutor

--- core/services/job_executor_test.go
+++ core/services/job_executor_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/smartcontractkit/chainlink/core/internal/cltest"
 	"github.com/smartcontractkit/chainlink/core/services"
+	"github.com/smartcontractkit/chainlink/core/store/assets"
 	"github.com/smartcontractkit/chainlink/core/store/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -28,6 +29,7 @@ func TestJobExecutor_Execute(t *testing.T) {
 	assert.NoError(t, store.CreateJob(&j))
 
 	run := j.NewRun(i)
+	run.Payment = assets.NewLink(9117)
 	require.NoError(t, store.CreateJobRun(&run))
 
 	err := je.Execute(run.ID)
@@ -39,6 +41,10 @@ func TestJobExecutor_Execute(t *testing.T) {
 	require.Len(t, run.TaskRuns, 2)
 	assert.Equal(t, models.RunStatusCompleted, run.TaskRuns[0].Status)
 	assert.Equal(t, models.RunStatusPendingConfirmations, run.TaskRuns[1].Status)
+
+	actual, err := store.LinkEarnedFor(&j)
+	require.NoError(t, err)
+	assert.Equal(t, assets.NewLink(9117), actual)
 }
 
 func TestJobExecutor_Execute_RunNotFoundError(t *testing.T) {
@@ -76,25 +82,3 @@ func TestJobExecutor_Execute_RunNotRunnableError(t *testing.T) {
 	err := je.Execute(run.ID)
 	require.Error(t, err)
 }
-
-// FIXME: need to test this somewhere more appropriate
-//func TestJobRunner_executeRun_correctlyAddsLinkEarnings(t *testing.T) {
-//store, cleanup := cltest.NewStore(t)
-//defer cleanup()
-
-//j := models.NewJob()
-//i := models.Initiator{Type: models.InitiatorWeb}
-//j.Initiators = []models.Initiator{i}
-//j.Tasks = []models.TaskSpec{
-//cltest.NewTask(t, "noop"),
-//}
-//assert.NoError(t, store.CreateJob(&j))
-//run := j.NewRun(i)
-//run.Payment = assets.NewLink(1)
-//require.NoError(t, store.CreateJobRun(&run))
-//require.NoError(t, services.ExportedExecuteRun(&run, store))
-
-//actual, err := store.LinkEarnedFor(&j)
-//require.NoError(t, err)
-//assert.Equal(t, assets.NewLink(1), actual)
-//}
