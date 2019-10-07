4d1542b29f3cf6428f9ac03e098d273e60a0a626
John Barker <dev@j16r.net>
2019-10-07 16:38:57 +0900

    Add sleeping test for sleep adapter

--- core/adapters/sleep_test.go
+++ core/adapters/sleep_test.go
@@ -13,6 +13,35 @@ import (
 func TestSleep_Perform(t *testing.T) {
 	store, cleanup := cltest.NewStore(t)
 	defer cleanup()
+	clock := cltest.NewTriggerClock()
+	store.Clock = clock
+
+	adapter := adapters.Sleep{}
+	err := json.Unmarshal([]byte(`{"until": 2147483647}`), &adapter)
+	assert.NoError(t, err)
+
+	doneChan := make(chan struct{})
+	go func() {
+		result := adapter.Perform(models.RunResult{}, store)
+		assert.Equal(t, string(models.RunStatusCompleted), string(result.Status))
+		doneChan <- struct{}{}
+	}()
+
+	select {
+	case <-doneChan:
+		t.Error("Sleep adapter did not sleep")
+	default:
+	}
+
+	clock.Trigger()
+
+	_, ok := <-doneChan
+	assert.True(t, ok)
+}
+
+func TestSleep_Perform_AlreadyElapsed(t *testing.T) {
+	store, cleanup := cltest.NewStore(t)
+	defer cleanup()
 
 	adapter := adapters.Sleep{}
 	err := json.Unmarshal([]byte(`{"until": 1332151919}`), &adapter)
